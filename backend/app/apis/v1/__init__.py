0ebe89cf95b418966992b411654b5e9cd23fcf9a
ice912 <tjs38390@naver.com>
2026-04-03 21:35:05 +0900

    Revert "fix: remove merge conflict markers in init file"
    This reverts commit e97a08d57295ac7d02ae2fb73750487cb5fe41f5.

--- backend/app/apis/v1/__init__.py
+++ backend/app/apis/v1/__init__.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
 
 from app.apis.v1.auth_routers import auth_router
-<<<<<<< HEAD
-=======
-from app.apis.v1.chat_ocr_routers import chat_ocr_router
->>>>>>> 497b069 (fix: remove merge conflict markers in init file)
 from app.apis.v1.ocr_routers import ocr_router
 from app.apis.v1.report_routers import report_router
 from app.apis.v1.user_routers import user_router
@@ -12,9 +8,5 @@ from app.apis.v1.user_routers import user_router
 v1_routers = APIRouter(prefix="/api/v1")
 v1_routers.include_router(auth_router)
 v1_routers.include_router(user_router)
-<<<<<<< HEAD
-=======
-v1_routers.include_router(chat_ocr_router)
->>>>>>> 497b069 (fix: remove merge conflict markers in init file)
 v1_routers.include_router(ocr_router)
 v1_routers.include_router(report_router)
