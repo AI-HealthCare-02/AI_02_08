8769bebbe2c2101cc728863256be676d1ffeb04b
ice912 <ID+username@users.noreply.github.com>
2026-04-06 14:40:18 +0900

    Any = Depends() 부분에 # noqa: B008을 추가하여 lake8-bugbear 등의 린트 경고를 비활성화

--- backend/app/apis/v1/__init__.py
+++ backend/app/apis/v1/__init__.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
 from app.apis.v1.auth_routers import auth_router
+from app.apis.v1.chat_ocr_routers import chat_ocr_router
 from app.apis.v1.ocr_routers import ocr_router
 from app.apis.v1.report_routers import report_router
 from app.apis.v1.user_routers import user_router
-from app.apis.v1.chat_ocr_routers import chat_ocr_router 
 
 v1_routers = APIRouter(prefix="/api/v1")
 v1_routers.include_router(chat_ocr_router, prefix="/chat", tags=["Chat OCR"])
